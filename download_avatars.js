1e7ee4336ecd3d86f059043d32c7921d89173885
Kristin <kvcmorin@gmail.com>
2017-03-14 07:37:59 +0000

    created a requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,14 @@
 var request = require('request');
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = 'violetreader';
+var GITHUB_TOKEN = 'cf46a8eff5c7535abed2cc1ad13eb39073c4a5b9';
 
 //cb is your ASYNCH callback function to handle results of your images in
 //each new file?
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 
