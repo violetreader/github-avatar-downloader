0286f80b93af69207ac878cd8dd0424ba67adfed
Kristin <kvcmorin@gmail.com>
2017-03-14 07:32:51 +0000

    created getRepContrib function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+//cb is your ASYNCH callback function to handle results of your images in
+//each new file?
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
