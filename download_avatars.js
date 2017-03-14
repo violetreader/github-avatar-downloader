55eb2c895b386e8607f2f628f74f10ba17f259f8
Kristin <kvcmorin@gmail.com>
2017-03-14 20:46:02 +0000

    grabbed the url value from the request page and stored each url as an image in their own file

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,5 @@
 var request = require('request');
-//do we need below????
-// var fs = require('fs');
+var fs = require('fs');
 
 
 var GITHUB_USER = 'violetreader';
@@ -10,8 +9,8 @@ var GITHUB_TOKEN = 'cf46a8eff5c7535abed2cc1ad13eb39073c4a5b9';
 //each new file?
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  // console.log(requestURL);
-
+  console.log(requestURL);
+  //typed in curl -I requestURL and couldnt find my added user-agent key-value pair there??
   request({url: requestURL, headers: {'User-Agent': 'request'}}, (error, response, body) => {
     if (error) {
       console.log('WTF, error', error);
@@ -21,25 +20,48 @@ function getRepoContributors(repoOwner, repoName, cb) {
       console.log('Response was not 200!', response);
       return false;
     }
-    let data = JSON.parse(body);
     // console.log('this is body', body);
-    console.log('this is data: ', data);
+    let data = JSON.parse(body);
+    // console.log('this is data: ', data);
 
     //if statement below is making sure data exist
     if (data && data.length) {
-
-      //write cb as a function and it wil b a function
-      data.forEach(cb);
+      // console.log('i am working');
+      var totalRequests = data.length;
+      data.forEach(aCallback);
     }
-    //this is where to write the cb functions bc its in scope where the param
-    //was created and also in the scope where its being referenced
+    function aCallback(contributor) {
+      console.log(contributor);
+      var avatURL = contributor.avatar_url;
+      var login = contributor.login;
+        //.on is throwing errors
+      request.get(avatURL)
+      .on('error', function(error) {
+        throw err;
+      })
+      .on('response', function(response){
+      // console.log('Downloading image...');
+      console.log('Response Status Message: ', response.statusMessage);
+      console.log('Response Headers: ', response.headers['content-type'])
+      // console.log('Download Complete.');
+      })
+      .on('end', function(end) {
+        totalRequests -= 1;
+        console.log('finished downloading ' + totalRequests)
+        // if(totalRequests === 0) {
+        //   cb(null, 'All images downloaded');
+        // }
+      })
+  //we must create a anonymas function that will create a new file for each image?
+      .pipe(fs.createWriteStream('./avatars/' + login + '.jpg'));
+    }
   })
 }
 
 //function (err, result) is the callback function?
 //or this param just needs to be a function, it can be any function?
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+getRepoContributors("jquery", "jquery", function(error, result) {
+  console.log("Errors:", error);
   console.log("Result:", result);
 });
 
